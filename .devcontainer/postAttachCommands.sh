dd16b9fd388f40d59f2080ebca9870409ee2ad95
Joerg Heinrich <contact@jfheinrich.eu>
2025-07-27 04:54:11 +0000

    feat: update devcontainer configuration to include shellcheck and improve path handling

--- .devcontainer/postAttachCommands.sh
+++ .devcontainer/postAttachCommands.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-(sudo apt update && sudo apt install keychain -y)
+(sudo apt update && sudo apt install keychain shellcheck -y)
 
 (gh auth status || gh auth login) && gh extension install https://github.com/nektos/gh-act
 
@@ -18,12 +18,17 @@
 npm ci
 
 /usr/bin/keychain \
---dir ~/.ssh/.keychain \
+--dir "$HOME"/.ssh/.keychain \
 --gpg2 --agents gpg,ssh \
-$(find ~/.ssh -name '*ed25519*' ! -iname '*.pub')
+"$(find "$HOME"/.ssh -name '*ed25519*' ! -iname '*.pub')"
 
-source ~/.ssh/.keychain/$HOSTNAME-sh
-source ~/.ssh/.keychain/$HOSTNAME-sh-gpg
+# shellcheck source=/home/vscode/.ssh/.keychain/$HOSTNAME-sh
+# shellcheck disable=SC1091
+source "$HOME"/.ssh/.keychain/"$HOSTNAME"-sh
+# shellcheck source=/home/vscode/.ssh/.keychain/$HOSTNAME-sh-gpg
+# shellcheck disable=SC1091
+source "$HOME"/.ssh/.keychain/"$HOSTNAME"-sh-gpg
 
 # Set GPG environment.
-export GPG_TTY=$(tty)
+GPG_TTY=$(tty)
+export GPG_TTY
